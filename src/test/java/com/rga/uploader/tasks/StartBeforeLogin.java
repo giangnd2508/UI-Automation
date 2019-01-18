d5b1cf4fc1982e7718fbcd4f52a2fc2f73b3079b
giang nguyen <giang.nd2508@gmail.com>
2019-01-18 19:57:17 +0700

    Update step for report

--- src/test/java/com/rga/uploader/tasks/StartBeforeLogin.java
+++ src/test/java/com/rga/uploader/tasks/StartBeforeLogin.java
@@ -7,11 +7,13 @@ import com.rga.uploader.ui.UploaderUI;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Open;
+import net.thucydides.core.annotations.Step;
 
 public class StartBeforeLogin implements Task {
 
 	UploaderUI theUploaderUI;
 
+	@Step("{0} open the page Started")
 	@Override
 	public <T extends Actor> void performAs(T actor) {
 		actor.attemptsTo(Open.browserOn(theUploaderUI));
