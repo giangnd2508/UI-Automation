b63ca752104a6a2ba7d310c0e3f8aa89b5939b83
giang nguyen <giang.nd2508@gmail.com>
2019-01-28 16:30:57 +0700

    Add ElementNotVisible exception

---
+++ src/main/java/com/rga/uploader/model/ElementNotVisible.java
@@ -0,0 +1,8 @@
+package com.rga.uploader.model;
+
+public class ElementNotVisible extends AssertionError {
+
+    public ElementNotVisible(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
